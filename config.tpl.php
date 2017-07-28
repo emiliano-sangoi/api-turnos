0104b669438e02346336386e0352de66bd62a139
Emiliano <emiliano.sangoi@gmail.com>
2017-07-28 20:36:35 -0300

    Cambio en config

--- config.tpl.php
+++ config.tpl.php
@@ -12,3 +12,5 @@ $config['db']['user']   = "root";
 $config['db']['pass']   = "";
 $config['db']['dbname'] = "";
 
+define('ROOT_URL', "http://" . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT']);
+define('SWAGGER_DOCS_URL_JSON', ROOT_URL . "/api/swagger/json");
