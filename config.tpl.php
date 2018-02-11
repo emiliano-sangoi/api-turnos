ba6e6fb205da40d7148e734e830a0cdd2749a0f1
Emiliano <emiliano.sangoi@gmail.com>
2018-02-11 12:29:58 -0300

    Modificacion del template del config.

--- config.tpl.php
+++ config.tpl.php
@@ -2,15 +2,26 @@
 
 /**
  * Variables de configuracion
+ * 
+ * Template generador a partir del archivo config.tpl.php
  */
 
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 
 $config['db']['host']   = "localhost";
-$config['db']['user']   = "root";
+$config['db']['user']   = "";
 $config['db']['pass']   = "";
 $config['db']['dbname'] = "";
+$config['host'] = $_SERVER['SERVER_NAME'];
+$config['port'] = $_SERVER['SERVER_PORT'];
+$config['path'] = '/api-turnos/api/index.php';
 
-define('ROOT_URL', "http://" . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT']);
-define('SWAGGER_DOCS_URL_JSON', ROOT_URL . "/api/swagger/json");
+/*
+ * Ejemplo:
+ * http://localhost/api-turnos/api/index.php/swagger/json
+ */
+
+
+define('ROOT_URL', "http://" . $config['host'] . ':' . $config['port'] . $config['path']);
+define('SWAGGER_DOCS_URL_JSON', ROOT_URL . "/swagger/json");
